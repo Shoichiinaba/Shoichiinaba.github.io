f6f0b1b3185089bca5561421ed6e9ae192ff3502
unknown <inabashoichi01@gmail.com>
2021-11-20 12:33:05 +0700

    update v2.0

--- js/onscroll.js
+++ js/onscroll.js
@@ -5,11 +5,11 @@ window.onscroll = function () {
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     navbar.classList.add("shadow");
-    navbar.classList.add("bg-light");
+    navbar.classList.add("bg-white");
     navbar.classList.remove("mt-3");
   } else {
     navbar.classList.remove("shadow");
-    navbar.classList.remove("bg-light");
+    navbar.classList.remove("bg-white");
     navbar.classList.add("mt-3");
   }
 }
